b701aa8298583016c7e29a7d7b9d37f1532a361a
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2023-04-28 12:12:00 +0530

    Add files via upload

---
+++ Asp.Net MVC Assignments/FootballLeague.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ado.netmvcAssignment.Models
+{
+    [Table("FootBallLeague")]
+
+    public class FootballLeague
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MatchId { get; set; }
+        public string TeamName1 { get; set; }
+        public string TeamName2 { get; set; }
+        public string Status { get; set; }
+        public int Points { get; set; }
+        public string WinningTeam { get; set; }
+    }
+}
---
+++ Asp.Net MVC Assignments/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ado.netmvcAssignment.Models;
+
+namespace Ado.netmvcAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext context=new MyDbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(context.FootBallLeague);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult AddMatchResult(FootballLeague a)
+        {
+            context.FootBallLeague.Add(a);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
