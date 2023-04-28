f0b6fa6bd567d87b7270037b70847174fd342129
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2023-04-28 12:30:25 +0530

    Add files via upload

---
+++ HOL6/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HandsOnLab6.Models;
+
+namespace HandsOnLab6.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+
+    }
+}
